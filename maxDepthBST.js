4ed7b8a3f989cbbe1480c01b48b6b40e51dffc4d
jennclee <8753352+jennclee@users.noreply.github.com>
2018-01-31 17:36:04 -0800

    Implement solution to maxDepthBST

--- maxDepthBST.js
+++ maxDepthBST.js
@@ -8,13 +8,16 @@ Given binary tree [3,9,20,null,null,15,7], depth is 3.
 */
 
 /*
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+  Definition for a binary tree node.
+  function TreeNode(val) {
+      this.val = val;
+      this.left = this.right = null;
+  }
  */
 
 const maxDepth = (root) => {
-
+  if (root === undefined || root === null) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
