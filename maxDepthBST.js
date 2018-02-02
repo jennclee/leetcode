2923c2ae909f0197ee8038c671723c0fb69df780
jennclee <8753352+jennclee@users.noreply.github.com>
2018-02-01 17:15:38 -0800

    DFS using stacks instead of recursion

--- maxDepthBST.js
+++ maxDepthBST.js
@@ -16,8 +16,33 @@ Given binary tree [3,9,20,null,null,15,7], depth is 3.
  */
 
 const maxDepth = (root) => {
-  if (root === undefined || root === null) {
-    return 0;
+  const stack = [];
+  stack.push(root);
+  let currentNode = stack[stack.length - 1];
+  let depthCounter = 0;
+  let maxDepth = 0;
+
+  while (stack.length > 0) {
+    const node = currentNode;
+    if (node.left) {
+      stack.push(node.left);
+      depthCounter++;
+    }
+    if (node.right) {
+      stack.push(node.right);
+      depthCounter++;
+    }
+    if (depthCounter > maxDepth) {
+      maxDepth = depthCounter;
+    }
+    currentNode = stack.pop();
   }
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  return depthCounter;
 };
+
+// const maxDepth = (root) => {
+//   if (root === undefined || root === null) {
+//     return 0;
+//   }
+//   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+// };
