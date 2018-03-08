6cbf8e9dfc47a94255e173d7c36e5348dbaa14df
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-07 19:08:51 -0800

    Add test cases for hasPathSum

--- binary-tree-intro/hasPathSum.js
+++ binary-tree-intro/hasPathSum.js
@@ -11,6 +11,14 @@
  * @return {boolean}
  */
 
+ /*
+=== TEST CASES ===
+[5,4,8,11,null,13,4,7,2,null,null,null,1]  22     // true  5 -> 4 -> 11 -> 2
+[1,2] 1                                           // false
+[-2,null,-3] -5                                   // true  -2 -> -3
+[1,-2,-3,1,3,-2,null,-1] -1                       // true
+ */
+
 const hasPathSum = (root, sum) => {
   // DFS method to find sum
   // stop if over sum
