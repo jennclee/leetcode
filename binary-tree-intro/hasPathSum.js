3a110c08a636d05531d05799f0f73ae8cd48c73a
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-07 19:10:12 -0800

    Implement more concise solution to hasPathSum to also handle negative sums and node values

--- binary-tree-intro/hasPathSum.js
+++ binary-tree-intro/hasPathSum.js
@@ -20,34 +20,12 @@
  */
 
 const hasPathSum = (root, sum) => {
-  // DFS method to find sum
-  // stop if over sum
-  // subtract node values from sum to determine if we can reach sum on that path
-  // only need to find one path
-  let pathExists = false;
-  if (root === null) {
-    pathExists = false;
-  } else {
-    if (sum - root.val === 0) {
-      if (!root.left && !root.right) {
-        pathExists = true;
-        return pathExists;
-      } else {
-        pathExists = false;
-      }
-    } else if (sum - root.val < 0) {
-      pathExists = false;
-    } else {
-      sum -= root.val;
-      const leftPath = hasPathSum(root.left, sum);
-      const rightPath = hasPathSum(root.right, sum);
-      if (leftPath) {
-        return true;
-      }
-      if (rightPath) {
-        return true;
-      }
-    }
+  if (root === null || root === undefined) {
+    return false;
   }
-  return pathExists;
+  sum -= root.val;
+  if (sum === 0 && root.left === null && root.right === null) {
+    return true;
+  }
+  return (hasPathSum(root.left, sum) || hasPathSum(root.right, sum));
 };
